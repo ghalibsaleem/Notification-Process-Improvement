b5f1671a9ffe20364786a457dd3252f49c41bf18
Ghalib saleem <ghalib.saleem.123@gmail.com>
2017-05-22 13:53:31 +0000

    Updated SMTPAsync.cs

--- Notification_PI/NetHelper/SMTPAsync.cs
+++ Notification_PI/NetHelper/SMTPAsync.cs
@@ -42,7 +42,7 @@ namespace Notification_PI.NetHelper
                     
                     smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
                     
-                    smtpClient.EnableSsl = true;
+                    smtpClient.EnableSsl = false;
                     smtpClient.UseDefaultCredentials = false;
                     smtpClient.Credentials = credentials;
                     ServicePointManager.ServerCertificateValidationCallback =
